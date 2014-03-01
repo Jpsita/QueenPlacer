03133a720940762683b3654b5ea41d1fe13bfeab
JPSgx <jpsdzn@gmail.com>
2014-03-01 16:40:06 +0100

    fixing resolver

--- QueenResolver/src/Resolver.java
+++ QueenResolver/src/Resolver.java
@@ -1,18 +1,18 @@
 public class Resolver {
-	private int x;
-	private int y;
+	private int startx;
+	private int starty;
 	private Object[][] table;
 	private int totQueens = 0;
 
 	public Resolver(int e_x, int e_y, Object[][] e_table) {
-		x = e_x;
-		y = e_y;
+		startx = e_x;
+		starty = e_y;
 		table = e_table;
 
 	}
 
 	public void Start() {
-		set(x, y);
+		set(startx, starty);
 
 	}
 
@@ -41,35 +41,38 @@ public class Resolver {
 
 	}
 
-	public boolean cont(int x, int y, int att) {
-		int lastx = x;
-		int lasty = y;
-		int attempt = att;
-		int stat = 0;
-		if (totQueens >= 8) {
-		for (int q = 0; q < 8; q++) {
-			for (int r = 0; r < 8; r++) {
-					if (table[q+attempt-1][r+attempt-1].equals(0)) {
-						set(q, r);
-						stat = 1;
-						totQueens += 1;
-						if (cont(q, r, 1) == true) {
-							return true;
-						}else{
-							totQueens -= 1;	
-							if(cont(lastx, lasty, 2) != true)
-							{
-							return false;	
+	public int cont(int x, int y, int att, Object[][] tbl) {
+		if (att <= 7) {
+			Object[][] temptbl = tbl;
+			int lastx = x;
+			int lasty = y;
+			int attempt = att;
+			int stat = 0;
+			if (totQueens >= 8) {
+				for (int q = 0; q < 8; q++) {
+					for (int r = 0; r < 8; r++) {
+						if (temptbl[q][r + attempt - 1].equals(0)) {
+							set(q, r);
+							stat = 1;
+							totQueens++;
+							if (cont(q, r, 1, temptbl) == 0) {
+								table = temptbl;
+								return 0;
+							} else if (cont(q, r, 1, temptbl) == 1) {
+								totQueens--;
+								if (cont(lastx, lasty, attempt + 1, tbl) == 1) {
+									return 1;
+								}
 							}
+
 						}
-						
 					}
 				}
+			} else {
+				return 0;
 			}
-		}else{
-			return true;
 		}
-		return false;
+		return 2;
 	}
 
 	public Object[][] getTable() {
